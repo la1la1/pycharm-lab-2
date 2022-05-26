503f2e6a0ce326d1c4bfeb5dfa8c388b9696182e
Lennon Lai Yao Cong <lennonlai08.21@ichat.sp.edu.sg>
2022-05-26 13:14:43 +0800

    Modified lab2

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,50 @@
-print("ET0735 (DevOps for AIoT) - Lab2 - Introduction to Python")
+def display_main_menu():
+    print("display_main_menu")
+
+
+def get_user_input():
+    listing = []
+    for i in range(7):
+        numbers = float(input("Please enter a number:"))
+
+        listing.append(numbers)
+
+    return listing
+
+
+def calc_average(numberset):
+    sum = 0
+    for i in range(0, 7):
+        sum = sum + numberset[i]
+    avg = sum / 7
+    return avg
+
+
+def find_min_max(numberset2):
+    min_max = []
+    maximum = max(numberset2)
+
+    # finding min
+
+    minimum = min(numberset2)
+
+    min_max.append(minimum)
+    min_max.append(maximum)
+    return min_max[0], min_max[1]
+
+
+def main():
+    display_main_menu()
+    num_list = get_user_input()
+    print(num_list)
+    average = calc_average(num_list)
+    print("The average is " + str(float(average)))
+    minimum_max = find_min_max(num_list)
+    minnumber = minimum_max[0]
+    maxnumber = minimum_max[1]
+    print("The minimum temperature is " + str(minnumber) + " and the maximum temperature "
+          + str(maxnumber))
+
+
+if __name__ == "__main__":
+    main()
